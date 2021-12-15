fdc117ca1d8e416131b92ef6fa9c476993a64cf5
rachi-development <43322@a.iesleopoldoqueipo.com>
2021-12-15 12:46:05 +0100

    Arreglando errores

--- Codigo/administrador/login/header2.php
+++ Codigo/administrador/login/header2.php
@@ -7,9 +7,6 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="../../inicio.php">Inicio</a>
-          <a class="navbar-brand" href="admin_portada.php">Gestión de usuarios</a>
-          <a class="navbar-brand" href="../../seccion/productos.php">Gestión de productos</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
